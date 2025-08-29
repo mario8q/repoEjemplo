109f1884fc0a568a67ae07acc9d3a3d301c4f002
Mario Santacruz <mariofernandosantacruzp@gmail.com>
2025-08-29 14:09:56 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoEjemplo",                               # Nombre del paquete
+    version="0.1.0",                                  # Versión inicial
+    packages=find_packages(),                         # Paquetes a incluir
+    description="Un paquete pip simple de saludo",    # Breve descripción
+    author="Mario Santa",                             # Tu nombre
+    author_email="mariofernandosantaruzp@gmail.com",  # Tu correo electrónico
+    url="https://github.com/mario8q/repoEjemplo",     # URL del proyecto
+)
